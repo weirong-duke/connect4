cd4fb0ffb36c1516208f4cbf3887dfddc17a4a38
Brice Leroy <bbrriiccee@gmail.com>
2020-01-11 13:51:22 -0800

    small optimization

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -16,9 +16,11 @@ export class Cell extends React.Component<Props> {
   // We want the winning cells to glow to show
   // the user the winning strike
   shouldGlow() {
-    const { winner, row, column } = this.props;
+    const { winner, row, column, color } = this.props;
 
-    if (!winner) {
+    // if there is no winner yet or this cell has not yet
+    // been played we won't have it glowing for sure
+    if (!winner || !color) {
       return false;
     }
 
