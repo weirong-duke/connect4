77a7ef031f397a0f7ff2da726fade602a99dd376
Brice Leroy <brice@friendbuy.com>
2020-01-15 10:25:26 -0800

    Updates

---
+++ precache-manifest.488dca32cefc590f0d5223d1a9e0b4fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4481e9d096b31c0632e58533b997336b",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "3f6706d5318f8966b4a0",
+    "url": "/connect4/static/css/main.caacecdf.chunk.css"
+  },
+  {
+    "revision": "4bcd4138b3554a320313",
+    "url": "/connect4/static/js/2.e54bf389.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/connect4/static/js/2.e54bf389.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f6706d5318f8966b4a0",
+    "url": "/connect4/static/js/main.d8fa0f82.chunk.js"
+  },
+  {
+    "revision": "c6f8aaad6b46ad746f10",
+    "url": "/connect4/static/js/runtime-main.3432deb0.js"
+  }
+]);
